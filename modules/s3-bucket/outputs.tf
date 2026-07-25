e35acb23b71251efa62fd16bd531db41d6f1a68a
Alexander Zakharov <alexander.zakharov@monetis.io>
2026-07-25 20:44:45 +0200

    feat(@modules/s3-bucket): update outputs

--- modules/s3-bucket/outputs.tf
+++ modules/s3-bucket/outputs.tf
@@ -1,15 +1,20 @@
 
-output "s3_bucket_arn" {
+output "arn" {
   description = "The ARN of the S3 bucket"
   value       = module.s3_bucket.s3_bucket_arn
 }
 
-output "s3_bucket_id" {
+output "id" {
   description = "The name of the S3 bucket"
   value       = module.s3_bucket.s3_bucket_id
 }
 
-output "s3_bucket_region" {
+output "region" {
   description = "The AWS region the S3 bucket resides in"
   value       = module.s3_bucket.s3_bucket_region
+}
+
+output "regional_domain_name" {
+  description = "Regional domain name of the S3 bucket"
+  value       = module.s3_bucket.s3_bucket_bucket_regional_domain_name
 }
